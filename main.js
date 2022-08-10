7c3094bd6a4fd53d2a9681bf90444903239f9fa3
A7337li <alizahra7337@gmail.com>
2022-08-10 23:12:18 +0430

    in the iran

--- main.js
+++ main.js
@@ -5,13 +5,14 @@ const list = $.querySelector(".list");
 let bool = true;
 
 menuburger.addEventListener('click', () => {
+
     if (bool == true) {
         menuburger.classList.add('open');
         list.style.top = 0;
         bool = false;
     } else {
         menuburger.classList.remove('open');
-        list.style.top = '-10%';
+        list.style.top = '-35%';
         bool = true;
     }
 })
